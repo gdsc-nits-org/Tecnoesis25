7681ada71d61432819574c7e4ced7b8217a6dba6
hirakRajbonshi <hrkraj0@gmail.com>
2025-08-31 03:13:33 +0530

    animatinon

---
+++ src/app/abc/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import Model from "~/components/VideoObj";
+
+export default function Page() {
+  // const [isAnimating, setIsAnimating] = useState(false);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setIsAnimating(false);
+  //   }, 10000);
+  // }, []);
+
+  return (
+    <div className="h-screen w-screen">
+      <Model />
+      <div
+        className="animate-fade-in absolute bottom-0 h-60 w-screen transition-transform delay-1000"
+        style={{ transform: "-translateY(10rem)" }}
+      >
+        <Image
+          className="absolute bottom-0 h-full w-full select-none"
+          src="/abc.svg"
+          width={300}
+          height={100}
+          alt="fkadshj"
+        />
+      </div>
+    </div>
+  );
+}
