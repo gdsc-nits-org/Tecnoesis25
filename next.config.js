77a7629ea1f507b0e16a632a017d9c5c192ba380
velgardey <arch.velgardey@gmail.com>
2025-09-06 01:13:49 +0530

    adds cdn links to assets

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "d3f6voaditlmqg.cloudfront.net",
+      },
+    ],
+  },
+};
 
 export default config;
