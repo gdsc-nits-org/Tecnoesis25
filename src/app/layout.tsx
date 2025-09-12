98047cf36e8883e020b38d7f6201667cbbf4544f
rishinhk004 <rishirajsaha555@gmail.com>
2025-09-12 11:22:27 +0530

    opengraph add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,29 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
+    <html
+      lang="en"
+      className={`${GeistSans.variable}`}
+      prefix="og: https://ogp.me/ns#"
+    >
+      <head>
+        <meta property="og:title" content="Tecnoesis 2025" />
+        <meta
+          property="og:description"
+          content="The Official Website of Tecnoesis 2025"
+        />
+        <meta
+          property="og:image"
+          content="https://tecnoesis25.pages.dev/tecnoesis25.png"
+        />
+        <meta property="og:image:width" content="300" />
+        <meta property="og:image:height" content="300" />
+        <meta
+          property="og:website"
+          content="https://tecnoesis25.pages.dev/"
+        />
+        <meta property="og:type" content="website" />
+      </head>
       <body className="min-h-screen flex flex-col no-scrollbar overflow-x-hidden">
         {children}
       </body>
