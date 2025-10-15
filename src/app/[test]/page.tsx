37b00be39d51c33dfba930ac6e37e1cfcf339c62
Newt-Shadow <anmol.s.sahoo@gmail.com>
2025-10-16 01:55:42 +0530

    gif

--- src/app/[test]/page.tsx
+++ src/app/[test]/page.tsx
@@ -1,9 +1,11 @@
+import Footer from "~/components/footer";
 import PhotoGallery from "~/components/gallery";
 
 export default function page(){
     return(
         <>
        <PhotoGallery />
+       <Footer />
         </>
     );
 }
--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -32,7 +32,7 @@ export default function PhotoGallery() {
           </Link>
         </div>
       </div>
-    <Footer />
+    
     </>
   );
 }
