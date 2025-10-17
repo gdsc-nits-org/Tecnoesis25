636e7d7b9a70682658adb592382d39a66bad3fb1
Barnil Sarma <barnilsarma@gmail.com>
2025-10-17 22:20:55 +0530

    nerds page layout changes

--- src/app/(main)/robotron/page.tsx
+++ src/app/(main)/robotron/page.tsx
@@ -1,11 +1,9 @@
 import { AboutSection, ModuleSection, TeamsSection,Hero, Footer } from '../../../components/robotron';
-import Navbar from '~/components/Navbar/Navbar';
 
 export default function RobotronPage() {
   return (
     <div className="min-h-screen bg-black text-white overflow-hidden">
       <main className="pb-0">
-        <Navbar/>
         <Hero/>
         <AboutSection />
         <ModuleSection />
