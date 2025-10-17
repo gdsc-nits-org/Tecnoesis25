efa91f5ffe567f3616074a9a6c3c6e5dcde09b4c
Sivangi77 <sivangikashyap9@gmail.com>
2025-10-17 15:24:21 +0530

    Sponsors section (#11)
    * Sponsors
    
    * Sponsors section done
    
    * Fix TS errors in sponsorpage
    
    * Fix sponsors
    
    * Sponsors fixed
    
    ---------
    
    Co-authored-by: rishinhk004 <rishirajsaha555@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import dynamic from "next/dynamic";
 const Gallery = dynamic(() => import("~/components/gallery"), { ssr: false });
 const About = dynamic(() => import("~/components/About"), { ssr: false });
+const PreviousSponsors = dynamic(() => import("~/components/PreviousSponsors"), { ssr: false });
 const Footer = dynamic(() => import("~/components/footer"), { ssr: false });
 
 
@@ -11,6 +12,7 @@ const Home = () => {
     <div className="flex flex-col min-h-screen overflow-x-hidden pt-28 md:pt-32 bg-black">
       <About />
       <Gallery />
+      <PreviousSponsors />
       <Footer />
     </div>
   );
