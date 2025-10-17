87bd0cbcc1a325415d3da270cd1d636e40183880
rishinhk004 <rishirajsaha555@gmail.com>
2025-10-17 15:15:53 +0530

    some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import dynamic from "next/dynamic";
 const Gallery = dynamic(() => import("~/components/gallery"), { ssr: false });
 const About = dynamic(() => import("~/components/About"), { ssr: false });
+const Footer = dynamic(() => import("~/components/footer"), { ssr: false });
 
 
 const Home = () => {
@@ -10,6 +11,7 @@ const Home = () => {
     <div className="flex flex-col min-h-screen overflow-x-hidden pt-28 md:pt-32 bg-black">
       <About />
       <Gallery />
+      <Footer />
     </div>
   );
 }
