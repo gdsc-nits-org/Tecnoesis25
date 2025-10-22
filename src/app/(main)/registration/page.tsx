8d47d64abab1a80869a17c162097fa889ff8b4fe
Dhruv Kumar <dhruv.k.dev@gmail.com>
2025-10-22 23:42:59 +0530

    Registration page 1 done

---
+++ src/app/(main)/registration/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Registration from '~/components/Registration'
+
+
+const RegistrationPage = () => {
+  
+  return (
+    <Registration />
+  )
+}
+
+export default RegistrationPage
