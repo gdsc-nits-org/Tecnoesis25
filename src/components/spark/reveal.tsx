0160f96a7a219687de79d9ac585a1831be8d27fa
Newt-Shadow <anmol.s.sahoo@gmail.com>
2025-10-26 00:03:39 +0530

    spark

--- src/components/spark/reveal.tsx
+++ src/components/spark/reveal.tsx
@@ -6,29 +6,29 @@ export default function SparkReveal() {
 
       <div className="absolute top-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center">
 
-        <div className="flex laptop:flex-row mobile:flex-col text-5xl tablet:text-7xl mobile2:scale-110 ipadpro:scale-125 laptop:scale-100 laptop:text-6xl laptop:gap-6 mobile:gap-0 items-center justify-center">
-          <p className="font-nyxerin bg-gradient-to-r from-[#520095] via-[#B46EFF] to-[#6200A9] bg-clip-text text-transparent">
+        <div className="flex laptop:flex-row mobile:flex-col text-5xl tablet:text-7xl  laptop:text-6xl laptop:gap-6 mobile:gap-0 items-center justify-center">
+          <p className="font-nyxerin bg-gradient-to-r from-[#520095] via-[#B46EFF] to-[#6200A9] bg-clip-text text-transparent mobile2:scale-110 ipadpro:scale-125 laptop:scale-100">
             OTHER
           </p>
-          <p className="font-nyxerin bg-gradient-to-r from-[#520095] via-[#B46EFF] to-[#6200A9] bg-clip-text text-transparent">
+          <p className="font-nyxerin bg-gradient-to-r from-[#520095] via-[#B46EFF] to-[#6200A9] bg-clip-text text-transparent mobile2:scale-110 ipadpro:scale-125 laptop:scale-100">
             ARTISTS
           </p>
         </div>
 
-        <div className="flex laptop:flex-row mobile:flex-col text-4xl tablet:text-7xl mobile2:scale-110 ipadpro:scale-125 ipadpro:pt-10 laptop:pt-2 laptop:scale-100 laptop:text-6xl laptop:gap-6 mobile:gap-0 pt-4 items-center justify-center">
-          <p className="font-nyxerin tracking-widest text-transparent" style={{ WebkitTextStroke: "0.5px white", WebkitTextFillColor: "transparent" }}>
+        <div className="flex laptop:flex-row mobile:flex-col text-4xl tablet:text-7xl ipadpro:pt-10 laptop:pt-2 laptop:text-6xl laptop:gap-6 mobile:gap-0 pt-4 items-center justify-center">
+          <p className="font-nyxerin tracking-widest text-transparent  mobile2:scale-110 ipadpro:scale-125  laptop:scale-100 " style={{ WebkitTextStroke: "0.5px white", WebkitTextFillColor: "transparent" }}>
             REVEALING
           </p>
-          <p className="font-nyxerin tracking-widest text-transparent" style={{ WebkitTextStroke: "0.5px white", WebkitTextFillColor: "transparent" }}>
+          <p className="font-nyxerin tracking-widest text-transparent  mobile2:scale-110 ipadpro:scale-125  laptop:scale-100 " style={{ WebkitTextStroke: "0.5px white", WebkitTextFillColor: "transparent" }}>
             SOON
           </p>
         </div>
 
-        <div className="relative flex flex-col text-9xl tablet:text-[10rem] laptop:text-9xl scale-[0.65] mobile2:scale-75 ipadpro:scale-105 ipadpro:-mt-1 laptop:scale-100 items-center -mt-6">
-          <p className="font-sefa rotate-[7.19deg] relative left-[-10%] bakbak-one-regular text-white">
+        <div className="relative flex flex-col text-9xl tablet:text-[10rem] laptop:text-9xl  ipadpro:-mt-1 laptop:scale-100 items-center -mt-6">
+          <p className="font-sefa rotate-[7.19deg] relative scale-[0.65] mobile2:scale-75 ipadpro:scale-105 left-[-10%] bakbak-one-regular text-white">
             स्पार्क
           </p>
-          <p className="font-sefa relative left-[26%] bg-gradient-to-r from-[#520095] via-[#B46EFF] to-[#6200A9] bg-clip-text text-transparent -mt-12">
+          <p className="font-sefa relative left-[26%] scale-[0.65] mobile2:scale-75 ipadpro:scale-105 bg-gradient-to-r from-[#520095] via-[#B46EFF] to-[#6200A9] bg-clip-text text-transparent -mt-12">
             Night
           </p>
         </div>
