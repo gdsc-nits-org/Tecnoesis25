097c0e9a668446850b0c4632cfb2dd58bc90cc90
rishinhk004 <rishirajsaha555@gmail.com>
2025-11-01 22:56:17 +0530

    fix

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -12,7 +12,7 @@ interface EventCardProps {
 
 function EventCard({ eventPoster, eventName, eventUrl, registrationUrl }: EventCardProps) {
   return (
-    <div className="group relative md:mb-6 h-full">
+    <div className="group relative md:mb-6 h-full ">
       <Image
         className="shadow-lgz-200 absolute left-1/2 top-28 sm:top-36 group-hover:z-20 w-44 sm:w-52 -translate-x-1/2 transform transition-all duration-500 group-hover:-translate-y-6"
         src={eventPoster}
@@ -34,7 +34,7 @@ function EventCard({ eventPoster, eventName, eventUrl, registrationUrl }: EventC
         width={400}
         height={300}
       />
-      <div className="z-30 hidden sm:flex flex-col items-center justify-center opacity-0 transition-all duration-500  group-hover:opacity-100">
+      <div className="z-30 hidden sm:flex flex-col items-center justify-center opacity-0 transition-all duration-500 translate-y-24 group-hover:opacity-100">
         <h1 className="font-nyxerin text-white">{eventName}</h1>
         <CustomButton text="Register" width={200} href={registrationUrl} />
         <CustomButton text="View Details" width={200} href={eventUrl} />
